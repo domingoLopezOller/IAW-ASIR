61a9de4f5b5fd25e2785437e7903483a0b035aca
domingoLopezOller <domingolopez.instituto@gmail.com>
2025-11-10 17:18:19 +0100

    rutas

--- reactvite/src/main.tsx
+++ reactvite/src/main.tsx
@@ -1,17 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Web from './webInstituto/Web.tsx'
+// import Web from './webInstituto/Web.tsx'
+// import Web2 from './webInstituto2/Web2.tsx';
+// import WebTarea from './tareaWeb/WebTarea.tsx';
 import Web2 from './webInstituto2/Web2.tsx';
 
 
-let nombre="Domingo lópez";
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     {/* <App /> */}
     
-    <Web/>
-    {/* <Web2/> */}
+    {/* <Web/> */}
+    <Web2/>
+    {/* <WebTarea/> */}
   </StrictMode>,
 
 
